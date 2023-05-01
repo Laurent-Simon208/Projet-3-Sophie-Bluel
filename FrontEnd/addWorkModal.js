3da7e49b8d3f99dc2aefc7ea0a8bd33d84393502
Laurent SIMON <simon.laurent208@gmail.com>
2023-05-01 15:12:59 +0200

    dernière version

--- FrontEnd/addWorkModal.js
+++ FrontEnd/addWorkModal.js
@@ -1,8 +1,8 @@
-const btnAddPicture = document.querySelector("#send-picture");
 const tokenVerification = localStorage.getItem("token");
 
-let modal = null;
 
+let modal = null;
+/**fonction pour ouvrir la modale */
 const openModal = function (e) {
   e.preventDefault();
   const target = document.querySelector(e.target.getAttribute("href"));
@@ -16,7 +16,7 @@ const openModal = function (e) {
     .querySelector(".js-modal-stop2")
     .addEventListener("click", stopPropagation);
 };
-/**créationde de la fonction pour fermer la modale */
+/**fonction pour fermer la modale */
 const closeModal = function (e) {
   if (modal === null) return;
   e.preventDefault();
@@ -40,14 +40,27 @@ document.querySelectorAll("#send-picture").forEach((a) => {
   a.addEventListener("click", openModal);
 });
 
+/**fonction pour fermer la modale apres l'envoi du fichier */
+function closeModalAfterAddPicture(){
+  const modalClose = document.querySelector("#modal2");
+  modalClose.style.display = "none";
+/**ajout de la réinitialisation de la couleur du bouton "valider" */
+  validateBtn.style.backgroundColor = "#B3B3B3";
+};
+
+
+/**fonction pour remettre le formulaire à zero */
 const resetForm = function () {
+  modalErrorMessage.style.display = "none";
   formSendProject.reset();
   previewImage.src = "";
   previewImage.style.display = "none";
   iconSendPicture.style.display = null;
   buttonAddPicture.style.display = null;
   textSendPicure.style.display = null;
+  validateBtn.style.backgroundColor = "#B3B3B3";
 };
+
 
 const formSendProject = document.querySelector("#form-send-project");
 const addPicture = document.querySelector("#add-picture");
@@ -58,33 +71,43 @@ const iconSendPicture = document.querySelector("#icon-send-picture");
 const textSendPicure = document.querySelector("#p-send-picture");
 const validateBtn = document.querySelector("#validate-btn");
 
-titlePicture.addEventListener("input", function () {
+/**ajout d'événements pour le que le bouton devienne vert */
+titlePicture.addEventListener("change", function () {
   if (
-    titlePicture.value &&
+    titlePicture.value != "" &&
     addPicture.files.length > 0 &&
-    categoryPicture.value !== ""
+    categoryPicture.value != ""
   ) {
     validateBtn.style.backgroundColor = "#1D6154";
   }
 });
-
 addPicture.addEventListener("change", function () {
-  if (titlePicture.value && addPicture.files.length > 0) {
+  if (
+    titlePicture.value != "" &&
+    addPicture.files.length > 0 &&
+    categoryPicture.value != ""
+  ) {
     validateBtn.style.backgroundColor = "#1D6154";
   }
 });
-
+categoryPicture.addEventListener("change", function () {
+  if (
+    titlePicture.value != "" &&
+    addPicture.files.length > 0 &&
+    categoryPicture.value != ""
+  ) {
+    validateBtn.style.backgroundColor = "#1D6154";
+  }
+});
+/**ajout de la prévisualisation de l'image à ajouter */
 buttonAddPicture.addEventListener("click", function (e) {
   e.preventDefault();
   addPicture.click();
 });
-
 const previewImage = document.querySelector("#preview-image");
-
 addPicture.addEventListener("change", function () {
   const file = this.files[0];
   const reader = new FileReader();
-
   reader.addEventListener(
     "load",
     function () {
@@ -92,7 +115,6 @@ addPicture.addEventListener("change", function () {
     },
     false
   );
-
   if (file) {
     previewImage.style.display = null;
     iconSendPicture.style.display = "none";
@@ -102,22 +124,31 @@ addPicture.addEventListener("change", function () {
   }
 });
 
+/**ajout du message d'erreur pour le remplissage des champs */
+const modalErrorMessage = document.querySelector("#modal-error");
+function errorMessage(){
+  modalErrorMessage.style.display = null;
+}
+
+
+/**ajout de nouveau projets */
 formSendProject.addEventListener("submit", function (e) {
   e.preventDefault();
   const formData = new FormData();
   formData.append("image", addPicture.files[0]);
   formData.append("title", titlePicture.value);
   formData.append("category", categoryPicture.value);
-
+/**si annulation réinitialisation du formulaire */
   if (
     addPicture.files.length === 0 ||
     titlePicture.value === "" ||
     categoryPicture.value === ""
   ) {
     console.log("erreur");
-    openModalErrorText();
+    errorMessage();
     return;
   } else {
+    /**envoi du projet dans l'api */
     fetch("http://localhost:5678/api/works", {
       method: "POST",
       body: formData,
@@ -135,7 +166,7 @@ formSendProject.addEventListener("submit", function (e) {
         buttonAddPicture.style.display = null;
         textSendPicure.style.display = null;
         console.log(data.imageUrl);
-
+        /**ajout du projet dans le dom de EditWorksModal */
         function addProjectWork(data) {
           const filesSection = document.querySelector(".sheets-projects");
 
@@ -151,6 +182,7 @@ formSendProject.addEventListener("submit", function (e) {
           projectElement.appendChild(pictureElement);
           projectElement.appendChild(nameElement);
         }
+        /**ajout du projet dans le dom de EditWorksHomePage */
         function addProjectModal(data) {
           const filesSectionMod = document.querySelector(".sheets-modal");
           const cardElement = document.createElement("article");
@@ -170,11 +202,11 @@ formSendProject.addEventListener("submit", function (e) {
           cardElement.appendChild(legendCard);
           cardElement.appendChild(deleteCard);
           deleteCard.appendChild(icon);
+
         }
         addProjectModal(data);
         addProjectWork(data);
-        closeModal2();
-        openModal1();
+        closeModalAfterAddPicture();
       })
       .catch((error) => {});
   }
